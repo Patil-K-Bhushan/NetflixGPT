5aece4a161e4ee6192f5efddf9b7fc61aaa2b2a8
Bhushan Patil <bpatil.00001@gmail.com>
2026-06-27 13:22:28 +0530

    Multi Language Support in GPT Search Page

--- netflix-gpt/src/components/Header.js
+++ netflix-gpt/src/components/Header.js
@@ -4,35 +4,38 @@ import { useNavigate } from "react-router-dom";
 import { auth } from "../utils/firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
-import NetflixGPT  from "../assets/NetflixGPT.png";
+import NetflixGPT from "../assets/NetflixGPT.png";
+import { toogleGptSearchView } from "../utils/gptSlice";
+import { SUPPORTED_LANGUAGES } from "../utils/constants";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
 
   useEffect(() => {
-      const unsubscribe = onAuthStateChanged(auth, (user) => {
-        if (user) {
-          const { uid, email, displayName, photoURL } = user;
-          dispatch(
-            addUser({
-              uid: uid,
-              email: email,
-              displayName: displayName,
-              photoURL: photoURL,
-            }),
-          );
-          navigate("/browse");
-        } else {
-          dispatch(removeUser());
-          navigate("/");
-        }
-      });
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          }),
+        );
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
 
-      return () => unsubscribe();
-    }, []);
-  
+    return () => unsubscribe();
+  }, []);
 
   const handleSignOut = () => {
     signOut(auth)
@@ -44,16 +47,38 @@ const Header = () => {
       });
   };
 
+  const handleGPTSearchClick = () => {
+    dispatch(toogleGptSearchView());
+  };
+
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between items-center">
-      <img
-        className="w-44"
-        src= {NetflixGPT}
-        alt="logo"
-      />
+      <img className="w-44" src={NetflixGPT} alt="logo" />
       {user && (
-        <div className="flex ">
-          <img className="w-8 h-8" src={user?.photoURL} alt="user-icon" />
+        <div className="flex p-2">
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <button
+            className="py-2 px-4 mx-4 my-2 text-white bg-purple-600 rounded-lg"
+            onClick={handleGPTSearchClick}
+          >
+            {showGptSearch ? "HomePage" : "GPT Search" }
+          </button>
+          <img className="w-12 h-12" src={user?.photoURL} alt="user-icon" />
           <button onClick={handleSignOut} className="font-bold text-white">
             {"Sign Out"}
           </button>
